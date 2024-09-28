4e347d850dcb6887f21b30a75f29a982eef40de9
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2024-09-27 23:40:10 -0400

    Time: 711 ms (79.25%), Space: 55.9 MB (44.28%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.py
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.py
@@ -7,11 +7,12 @@ class Solution:
     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
         num_set = set(nums)
         dummy = ListNode(0)
-        dummy.next = head
+        dummy.next = head ## don't forget this step
         prev = dummy
         curr = head
         while curr:
             if curr.val in num_set:
+                ## In this case prev does not move
                 prev.next = curr.next
                 curr = curr.next
             else:
