71a18620ebdb5f270357259bae54178760fcc2e3
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2024-10-08 23:31:01 -0400

    Time: 725 ms (5.08%), Space: 31.4 MB (15.97%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.py
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.py
@@ -14,11 +14,13 @@ class Solution:
                 max_num = nums[i]
                 curr_len = 1 ## both current window and
                 max_len = 1 ## max length are reset as we have new max_num
-            else:
-                ## when nums[i] == max_num
+            elif nums[i] == max_num:
+                ## Case when we are on a continuous streak of the max_num
                 if nums[i] == nums[i-1]:
                     curr_len += 1
                 else:
+                    ## Case when we are seeing the max_num again after 
+                    ## seeing some other number
                     curr_len = 1
                 max_len = max(max_len,curr_len)
             i += 1
