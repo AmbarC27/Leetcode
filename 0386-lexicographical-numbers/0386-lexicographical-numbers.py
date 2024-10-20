12522e8a58f4c90a80c1b55734dd0f625af6f8bf
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2024-10-20 03:52:13 -0400

    Time: 391 ms (5.01%), Space: 21.8 MB (99.4%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -2,8 +2,6 @@ class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
         digit_length = len(str(n))
         ans = []
-        # starting_num = "1" + "0"*(digit_length - 1)
-        # for digit in "123456789":
         def generate(curr_num):
             if len(curr_num) > digit_length:
                 return
@@ -11,11 +9,9 @@ class Solution:
                 ans.append(int(curr_num))
             for digit in "0123456789":
                 generate(curr_num + digit)
+
+        ## Starting backtrakcing of first digit outside the backtracking function
+        ## to avoid having a number beginning with a zero
         for digit in "123456789":
             generate(digit)
         return ans
-
-        # while len(ans) < n:
-        #     for digit in "0123456789":
-
-        #     for length in range(1,n+1):
