f50ed2670b3de87f8939c849a24348705b2dad8d
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2025-01-14 22:45:50 -0500

    Time: 144 ms (60.71%), Space: 19 MB (7.08%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.py
@@ -1,8 +1,8 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
         freq = Counter(s)
-        ans = 0
+        pairs_removed = 0
         for char in freq:
             if freq[char] >= 3:
-                ans += (freq[char] - 1)//2
-        return len(s) - 2*ans
+                pairs_removed += (freq[char] - 1)//2
+        return len(s) - 2*pairs_removed
