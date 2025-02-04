a0a9126668cd68326af34a7f22aa6ab3ceab685e
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2025-02-03 21:08:59 -0500

    Time: 95 ms (37.09%), Space: 22.3 MB (63.64%) - LeetHub

--- 1816-lowest-common-ancestor-of-a-binary-tree-iv/1816-lowest-common-ancestor-of-a-binary-tree-iv.py
+++ 1816-lowest-common-ancestor-of-a-binary-tree-iv/1816-lowest-common-ancestor-of-a-binary-tree-iv.py
@@ -21,9 +21,8 @@ class Solution:
                     queue.append(node.right)
             return nodes_found == len(nodes_to_search)
 
-
         lca = root
-        while lca:
+        while True:
             found_in_left = False
             found_in_right = False
             if lca.left:
@@ -38,27 +37,3 @@ class Solution:
                 ## All nodes are not found in any child so current node
                 ## is lca
                 return lca
-        # node_vals = set()
-        # node_queue = collections.deque()
-        # node_queue.append(root)
-        # while node_queue:
-        #     node = node_queue.popleft()
-        #     node_vals.add(node.val)
-        #     if node.left:
-        #         node_queue.append(node.left)
-        #     if node.right:
-        #         node_queue.append(node.right)
-        # print(node_vals)
-
-        # queue = collections.deque()
-        # queue.append(root)
-        # lca = root
-        # while queue:
-        #     node = queue.popleft()
-        #     if search_nodes(node,node_vals):
-        #         lca = node
-        #     if node.left:
-        #         queue.append(node.left)
-        #     if node.right:
-        #         queue.append(node.right)
-        # return lca
