728e342ca26728a9dc27f98fa6cbb00e44343951
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2025-03-20 14:47:09 -0400

    Time: 3 ms (87.76%), Space: 18.2 MB (38.16%) - LeetHub

--- 0779-max-chunks-to-make-sorted-ii/0779-max-chunks-to-make-sorted-ii.py
+++ 0779-max-chunks-to-make-sorted-ii/0779-max-chunks-to-make-sorted-ii.py
@@ -1,15 +1,29 @@
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
-        sorted_arr = sorted(arr)
-        ## Need a monotonic non-decreasing stack
-        # stack = []
+        # sorted_arr = sorted(arr)
+        # chunks = 0
         # for i in range(len(arr)):
-        #     while stack and stack[-1] >= arr[i]:
-        #         stack.pop()
-        #     stack.append(arr[i])
-        # return len(stack)
+        #     if sorted(arr[:i+1]) == sorted_arr[:i+1]:
+        #         chunks += 1
+        # return chunks
+
+        ## This approach is editorial 1 of Leetcode 769, which is the first
+        ## question in this series of questions
+        n = len(arr)
+        max_left = [0] * n
+        min_right = [0] * n
+        
+        max_left[0] = arr[0]
+        for i in range(1, n):
+            max_left[i] = max(max_left[i - 1], arr[i])
+        
+        min_right[-1] = arr[-1]
+        for i in range(n - 2, -1, -1):
+            min_right[i] = min(min_right[i + 1], arr[i])
+        
         chunks = 0
-        for i in range(len(arr)):
-            if sorted(arr[:i+1]) == sorted_arr[:i+1]:
+        for i in range(n - 1):
+            if max_left[i] <= min_right[i + 1]:
                 chunks += 1
-        return chunks
+
+        return chunks + 1
