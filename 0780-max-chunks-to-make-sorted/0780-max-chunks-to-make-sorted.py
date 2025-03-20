15827c52985bc42e0a1ecef0dab64dd865316529
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2025-03-20 13:45:05 -0400

    Time: 0 ms (100%), Space: 17.7 MB (48.51%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.py
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.py
@@ -2,6 +2,10 @@ class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
         chunks = 0
         max_in_current_chunk = -1
+
+        ## We want that in the array range(beginning of chunk,end of chunk) we
+        ## have all the numbers in the range(beginning of chunk,end of chunk).
+        ## Every time we accomplish that, we have found a new chunk
         for i in range(len(arr)):
             max_in_current_chunk = max(max_in_current_chunk,arr[i])
             if i == max_in_current_chunk:
