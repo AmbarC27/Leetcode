77966f8473f48e19d4e5cd51c933caff56a43bc0
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2025-03-20 13:21:46 -0400

    Time: 11 ms (86.28%), Space: 30.9 MB (78.22%) - LeetHub

--- 1909-buildings-with-an-ocean-view/1909-buildings-with-an-ocean-view.py
+++ 1909-buildings-with-an-ocean-view/1909-buildings-with-an-ocean-view.py
@@ -1,8 +1,18 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
-        stack = []
-        for i in range(len(heights)):
-            while stack and heights[stack[-1]] <= heights[i]:
-                stack.pop()
-            stack.append(i)
-        return stack
+        ## Going from the left to right
+        # stack = []
+        # for i in range(len(heights)):
+        #     while stack and heights[stack[-1]] <= heights[i]:
+        #         stack.pop()
+        #     stack.append(i)
+        # return stack
+
+        ## Going from the right to left
+        ans = []
+        for i in range(len(heights)-1,-1,-1):
+            if not ans:
+                ans.append(i)
+            if heights[i] > heights[ans[-1]]:
+                ans.append(i)
+        return ans[::-1]
