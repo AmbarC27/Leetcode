2c545f3c618baf524b4463ff737b27bd9c803337
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2025-03-20 17:28:11 -0400

    Time: 19 ms (97.75%), Space: 18 MB (93.66%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        m = len(grid) ## vertical
+        n = len(grid[0]) ## horizontal
+        max_area = 0
+        for j in range(m):
+            for i in range(n):
+                if grid[j][i] == 1:
+                    queue = collections.deque()
+                    queue.append([i,j])
+                    area = 0
+                    grid[j][i] = 0 ## marking it so that we dont process it again
+                    while queue:
+                        area += 1
+                        x,y = queue.popleft()
+                        for dx,dy in [[0,1],[0,-1],[1,0],[-1,0]]:
+                            nx = x + dx
+                            ny = y + dy
+                            if (0 <= nx < n) and (0 <= ny < m) and grid[ny][nx] == 1:
+                                queue.append([nx,ny])
+                                grid[ny][nx] = 0 ## marking it so that we dont process it again
+                    max_area = max(max_area,area)
+        return max_area
