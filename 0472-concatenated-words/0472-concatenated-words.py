81b90f457672f41e1ead3523f5d68165fa9de67d
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2025-03-24 01:38:28 -0400

    Time: 587 ms (9.72%), Space: 19.8 MB (96.32%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.py
+++ 0472-concatenated-words/0472-concatenated-words.py
@@ -4,20 +4,31 @@ class Solution:
         words_set = set(words)
         for word in words:
             dp = [False]*(len(word) + 1)
-            dp[0] = True ## Set empty substring as true as you can always form it
+            dp[-1] = True ## Set empty substring as true as you can always form it
             
             ## Remove the current word from the set as otherwise we will always process
             ## that the current word is a substring of itself and fasely append to ans
             words_set.remove(word)
-            for r in range(len(word)+1):
-                for l in range(r):
+
+            ## Either for loop works
+            ## If this for loop changes, need to change lines 7 and 32
+            # for r in range(len(word)+1):
+            #     for l in range(r):
+            #         if word[l:r] in words_set:
+            #             dp[r] = dp[l]
+            #         if dp[r]:
+            #             break
+
+            ## Either for loop works
+            for l in range(len(word),-1,-1):
+                for r in range(l+1,len(word)+1):
                     if word[l:r] in words_set:
-                        dp[r] = dp[l]
-                    if dp[r]:
+                        dp[l] = dp[r]
+                    if dp[l]:
                         break
             
             ## Add the word back once processing is done
             words_set.add(word)
-            if dp[-1]:
+            if dp[0]:
                 ans.append(word)
         return ans
