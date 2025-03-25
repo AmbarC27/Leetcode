fc85452dcc5c7e4802ec7ee39e9254e1ee9120bc
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2025-03-25 19:11:58 -0400

    Time: 22 ms (6.11%), Space: 18.4 MB (14.35%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        if len(flowerbed) == 1:
+            if flowerbed[0] == 0:
+                return n <= 1
+            else:
+                return n == 0
+
+        for i in range(len(flowerbed)):
+            if i == 0:
+                if flowerbed[i] == 0 and flowerbed[i+1] == 0:
+                    n -= 1
+                    flowerbed[i] = 1
+            elif i == len(flowerbed) - 1:
+                if flowerbed[i] == 0 and flowerbed[i-1] == 0:
+                    n -= 1
+                    flowerbed[i] = 1
+            else:
+                ## We check the two adjacent plots
+                if flowerbed[i-1:i+2] == [0,0,0]:
+                    flowerbed[i] = 1
+                    n -= 1
+        return n <= 0
