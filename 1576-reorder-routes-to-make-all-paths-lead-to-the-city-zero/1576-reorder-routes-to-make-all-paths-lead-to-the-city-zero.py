595ca3edf3ac1242b27611345ad17628eb7ff299
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2025-03-26 17:46:39 -0400

    Time: 227 ms (29.33%), Space: 54.7 MB (9.71%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,5 +1,12 @@
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        ## Key to this question is that we create an undirected graph out
+        ## of the initial directed graph. Original edges will have a cost 
+        ## of 1 and the new edges will have a cost of 0. As we want to change
+        ## roads which lead away from zero, we start traversing from zero
+        ## (either BFS or DFS) and we find the cost to traverse the entire graph
+        ## The original edges which lead away from zero will add 1 to the cost
+        ## each time it is traversed 
         adj_dict = {}
         for u,v in connections:
             if u not in adj_dict:
