0c5d1a99fbfb1ebe9b9639c903bf6041b6e0bfb8
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2025-03-25 20:57:59 -0400

    Time: 82 ms (83.58%), Space: 18.1 MB (74.37%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
@@ -2,7 +2,8 @@ class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
         ## We need to check if the counter frequencies of each word are the same
         ## (We dont care which particular element has what count, just that
-        ## frequencies should match)
+        ## frequencies should match) as long as character set of each word is
+        ## the same
         counter1 = Counter(word1)
         counter2 = Counter(word2)
 
@@ -12,4 +13,3 @@ class Solution:
         if char_set1 == char_set2:
             return sorted(list(counter1.values())) == sorted(list(counter2.values()))
         return False
-        # return counter1 == counter2
