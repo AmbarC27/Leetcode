4a6978fcd76b9093d37808e70edaa698dd2c29b9
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2025-03-25 20:18:18 -0400

    Time: 503 ms (69.76%), Space: 29.6 MB (95.76%) - LeetHub

---
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        # hashmap = Counter(nums)
+        # ans = 0
+        # for num in hashmap.keys():
+        #     complement = k - num
+        #     if num in hashmap and complement in hashmap:
+        #         if num != complement:
+        #             if hashmap[num] >= 1 and hashmap[complement] >= 1:
+        #                 ans += 1
+        #                 hashmap[num] -= 1
+        #                 hashmap[complement] -= 1
+        #         elif num == complement:
+        #             if hashmap[num] >= 2:
+        #                 ans += 1
+        #                 hashmap[num] -= 2
+        # return ans
+        nums.sort()
+        l = 0
+        r = len(nums) - 1
+        ans = 0
+        while l < r:
+            if nums[l] + nums[r] == k:
+                ans += 1
+                l += 1
+                r -= 1
+            elif nums[l] + nums[r] < k:
+                l += 1
+            elif nums[l] + nums[r] > k:
+                r -= 1
+        return ans
