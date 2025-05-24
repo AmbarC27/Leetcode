f5f58e0b3d07bccc215d2d647ce250ea8d278117
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2025-05-24 15:18:29 -0400

    Time: 327 ms (30.49%), Space: 66.7 MB (5.04%) - LeetHub

---
+++ 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        min_time = {}
+        adj_dict = {} ## DAG mapping manager to subordinates
+        # for employee_num in manager:
+        #     adj_dict[manager[employee_num]]  = adj_dict.get(manager[employee_num],[]) + [employee_num]
+        for employee_num in range(len(manager)):
+            adj_dict[manager[employee_num]]  = adj_dict.get(manager[employee_num],[]) + [employee_num]
+        
+        def dfs(employee,time):
+            min_time[employee] = time
+            if employee in adj_dict:
+                for subordinate in adj_dict[employee]:
+                    dfs(subordinate,time + informTime[subordinate])
+
+        dfs(headID,informTime[headID])
+        return max(min_time.values())
