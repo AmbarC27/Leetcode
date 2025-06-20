b6da7929eb8ee0eda66230946b41717cdbbaabeb
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2025-06-20 11:30:50 -0400

    Time: 86 ms (57.51%), Space: 21.2 MB (68.01%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,63 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+
+        ## Note that you have to use either dfs for both serialize and deserialize,
+        ## or bfs. Can't be mixed and matched as the structure of deserialization has
+        ## to match the structure of serialization (where structure is either bfs or dfs)
+        res = []
+
+        def dfs(node):
+            if not node:
+                res.append("N")
+                return
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+        return ",".join(res)
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        vals = data.split(",")
+        ## Note we can't do without keeping a track of index i, it is not as
+        ## simple as incrementing it when done processing one node, because
+        ## we do not know by how much it would increment after dfs is performed
+        ## on node.left (line 53)
+        self.i = 0
+
+        def dfs():
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            node.left = dfs()
+            node.right = dfs()
+            return node
+
+        return dfs()
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
