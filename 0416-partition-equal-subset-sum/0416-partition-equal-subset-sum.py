d9f222ed89d12e20f78aacf8470545241d62f390
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2025-09-24 19:56:11 -0400

    Time: 1392 ms (33.09%), Space: 83.1 MB (17.92%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        tot_sum = sum(nums)
+        if tot_sum % 2 == 1:
+            return False
+
+        subset_sum = tot_sum // 2
+        
+        ## As backtracking keeps track of two variables (remaining_sum
+        ## and index, cache also needs to be two dimensional)
+        cache = [[None] * (len(nums)) for _ in range(subset_sum+1)]
+
+        def backtrack(remaining_sum,i,n):
+            if remaining_sum < 0 or i == n:
+                return False
+            if cache[remaining_sum][i] != None:
+                return cache[remaining_sum][i]
+            if remaining_sum == 0:
+                cache[remaining_sum][i] = True
+                return True
+            
+            ## You can either decide to add element i to the current 
+            ## subset which you are building, or omit it
+            result = (backtrack(remaining_sum, i+1, n) or
+                    backtrack(remaining_sum - nums[i], i+1, n))
+            cache[remaining_sum][i] = result
+            return result
+
+        return backtrack(subset_sum, 0, len(nums))
