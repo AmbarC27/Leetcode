b21fff329417c19ad7b21abb92949e792ea2b768
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2025-10-25 11:16:25 -0400

    Time: 0 ms (100%), Space: 18.6 MB (26.67%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,27 +1,43 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        l = 0
-        r = len(nums) - 1
+        # l = 0
+        # r = len(nums) - 1
+        # while l <= r:
+        #     mid = (l+r)//2
+        #     if nums[mid] == target:
+        #         return mid
+        #     elif nums[mid] < target:
+        #         l = mid + 1
+        #     else:
+        #         r = mid - 1
+
+        # ## case when we haven't found it, thus need to insert it
+
+        # ## insert in the left-most position (could have also chosen
+        # ## right-most)
+
+        # l = 0
+        # r = len(nums) - 1
+        # while l <= r:
+        #     mid = (l+r)//2
+        #     if nums[mid] < target:
+        #         l = mid + 1
+        #     if nums[mid] >= target:
+        #         r = mid - 1
+        # return l
+
+        ## This one pass also works
+        l, r = 0, len(nums) - 1
         while l <= r:
-            mid = (l+r)//2
+            mid = (l + r) // 2
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
                 l = mid + 1
             else:
                 r = mid - 1
+        return l  # first index where nums[idx] >= target
 
-        ## case when we haven't found it, thus need to insert it
-
-        ## insert in the left-most position (could have also chosen
-        ## right-most)
-
-        l = 0
-        r = len(nums) - 1
-        while l <= r:
-            mid = (l+r)//2
-            if nums[mid] < target:
-                l = mid + 1
-            if nums[mid] >= target:
-                r = mid - 1
-        return l
+        ## Why return l works: at exit, r is the last index < target
+        ## and l is the first index ≥ target (the correct insertion
+        ## position)
