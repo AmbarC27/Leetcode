f6962b954bdccea42252b109b4c9f8cf01227094
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2025-10-26 21:48:31 -0400

    Time: 250 ms (84.13%), Space: 59 MB (79.34%) - LeetHub

--- 1977-minimum-interval-to-include-each-query/1977-minimum-interval-to-include-each-query.py
+++ 1977-minimum-interval-to-include-each-query/1977-minimum-interval-to-include-each-query.py
@@ -8,9 +8,15 @@ class Solution:
         heapq.heapify(minheap)
         i = 0
 
+        ## Think of it as two steps:
+        ## 1) Push all intervals which may potentially include query by putting all
+        ## intervals whose start point is lesser than query (being overly optimistic)
+        ## 2) Go from overly optimistic to realistic by removing all queries whose end
+        ## point doesn't cover the query
         for query in sorted_queries:
             ## Push intervals into the heap as long as its start point is
-            ## lesser than or equal to the query
+            ## lesser than or equal to the query, and so the interval may 
+            ## potentially contain query
             while i < len(intervals) and intervals[i][0] <= query:
                 l, r = intervals[i]
                 ## Add end point of interval into the heap too as that will
@@ -26,6 +32,9 @@ class Solution:
             ## If minheap is not empty that means there are intervals which 
             ## contain query, and thus choose the min length interval. If not,
             ## then return -1 as the question asks
+
+            ## At this point the minheap will only contain intervals which
+            ## contain query
             ans_dict[query] = minheap[0][0] if minheap else -1
 
         return [ans_dict[q] for q in queries]
