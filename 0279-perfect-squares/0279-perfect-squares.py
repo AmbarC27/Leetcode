758afe9e8f6208a5dd35d8c37902c4af0622e72e
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2025-10-29 00:30:40 -0400

    Time: 5503 ms (5.01%), Space: 21.4 MB (19.94%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -8,7 +8,6 @@ class Solution:
             highest_number_to_be_squared = math.floor(math.sqrt(target))
             if target == highest_number_to_be_squared ** 2:
                 return 1
-            # ans = float("inf")
             for num in range(1,highest_number_to_be_squared + 1):
                 dp[target] = min(dp[target],1 + numways(target - num**2))
             return dp[target]
