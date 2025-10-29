b7408fa9dc0761ed07220b747dfabe11a43ce102
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2025-10-29 00:30:24 -0400

    Time: 5533 ms (5.01%), Space: 21.3 MB (20.03%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [float("inf")] * (n+1)
+        
+        def numways(target):
+            if dp[target] < float("inf"):
+                return dp[target]
+            highest_number_to_be_squared = math.floor(math.sqrt(target))
+            if target == highest_number_to_be_squared ** 2:
+                return 1
+            # ans = float("inf")
+            for num in range(1,highest_number_to_be_squared + 1):
+                dp[target] = min(dp[target],1 + numways(target - num**2))
+            return dp[target]
+
+        return numways(n)
