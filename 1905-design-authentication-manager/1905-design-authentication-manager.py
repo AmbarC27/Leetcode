84b7292b295b6074cf5ed29a0260b16353a35138
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2025-11-01 19:28:32 -0400

    Time: 11 ms (91.13%), Space: 19.6 MB (15.46%) - LeetHub

---
+++ 1905-design-authentication-manager/1905-design-authentication-manager.py
@@ -0,0 +1,33 @@
+class AuthenticationManager:
+
+    def __init__(self, timeToLive: int):
+        self.ttl = timeToLive
+        self.minheap = []
+        self.token_to_ttl = {}
+        heapq.heapify(self.minheap)
+
+    def generate(self, tokenId: str, currentTime: int) -> None:
+        self.token_to_ttl[tokenId] = currentTime + self.ttl
+        heapq.heappush(self.minheap, (currentTime + self.ttl,tokenId))
+
+    def renew(self, tokenId: str, currentTime: int) -> None:
+        if tokenId in self.token_to_ttl:
+            if currentTime < self.token_to_ttl[tokenId]:
+                self.token_to_ttl[tokenId] = currentTime + self.ttl
+                heapq.heappush(self.minheap, (currentTime + self.ttl,tokenId))
+
+
+    def countUnexpiredTokens(self, currentTime: int) -> int:
+        while self.minheap and self.minheap[0][0] <= currentTime:
+            expiry_time, token = heapq.heappop(self.minheap)
+            if expiry_time == self.token_to_ttl[token]:
+                del self.token_to_ttl[token]
+        return len(self.token_to_ttl)
+        
+
+
+# Your AuthenticationManager object will be instantiated and called as such:
+# obj = AuthenticationManager(timeToLive)
+# obj.generate(tokenId,currentTime)
+# obj.renew(tokenId,currentTime)
+# param_3 = obj.countUnexpiredTokens(currentTime)
