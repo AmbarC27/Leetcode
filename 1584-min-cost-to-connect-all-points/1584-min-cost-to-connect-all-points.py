042de143cf9e4f1692bd1459884a51ab62b73cb7
Ambar Chakraborty <70912291+AmbarC27@users.noreply.github.com>
2026-01-19 23:10:40 -0500

    Time: 4159 ms (8.09%), Space: 77.4 MB (78.26%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -0,0 +1,36 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        ## Create a complete graph of all the points
+        adj_dict = {}
+        for i in range(len(points)):
+            for j in range(len(points)):
+                nodes = []
+                if i != j:
+                    nodes.append(points[j])
+            adj_dict[tuple(points[i])] = points
+        ## In heap first value is the edge weight and second value is the 
+        ## next node. Our initial next node would be the first node in points
+        heap = [[0,points[0]]]
+        heapq.heapify(heap)
+        visited = set()
+        total = 0
+        while heap:
+            weight,curr_node = heapq.heappop(heap)
+            if tuple(curr_node) in visited:
+                ## Node is already in MST, so skip it to avoid cycles
+                continue
+            
+            ## Case when node is not in MST
+            total += weight
+            visited.add(tuple(curr_node))
+            for next_node in adj_dict[tuple(curr_node)]:
+                ## the "not in visited" check isn't necessary here 
+                ## but its an optimization
+                if tuple(next_node) not in visited:
+                    x1 = curr_node[0]
+                    x2 = next_node[0]
+                    y1 = curr_node[1]
+                    y2 = next_node[1]
+                    dist = abs(x1-x2)+abs(y1-y2)
+                    heapq.heappush(heap,[dist,next_node])
+        return total
